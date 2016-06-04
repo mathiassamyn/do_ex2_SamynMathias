f146b665c931d4f2eaa1650bef133bce559c994d
mathiassamyn <mathiassamyn@hotmail.com>
2016-06-04 23:42:09 +0200

    vagrant provisioning of mgr

--- Vagrantfile
+++ Vagrantfile
@@ -27,7 +27,7 @@ Vagrant.configure(2) do |config|
 	    node.vm.hostname = "www#{i}.dev"
             node.vm.network :private_network, ip: "192.168.2.3#{i}"
             node.vm.network :forwarded_port, guest: 22, host: 2222, id: "ssh", disabled: true
-            node.vm.network :forwarded_port, guest: 22, host: 333#{i}, auto_correct: true
+            node.vm.network :forwarded_port, guest: 22, host: "333#{i}", auto_correct: true
 	end
      end
 
--- ansible.sh
+++ ansible.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
+
+# install ansible (http://docs.ansible.com/intro_installation.html)
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible sshpass
 
+ln -sf /vagrant/hostnames /etc/hosts
+
 ln -sf /vagrant/hosts /etc/ansible/hosts
 
-rm /home/vagrant/.ssh/known_host
+rm /home/vagrant/.ssh/known_hosts
 
-KNOW_HOSTS_FILE="/home/vagrant/.ssh/known_hosts"
+ssh-keyscan lb www1 www2 mgr >> /home/vagrant/.ssh/known_hosts
+chown vagrant:vagrant /home/vagrant/.ssh/known_hosts
 
-ssh-keyscan 192.168.2.2 >> $KNOW_HOSTS_FILE 
-ssh-keyscan 192.168.2.4 >> $KNOW_HOSTS_FILE
-ssh-keyscan localhost >> $KNOW_HOSTS_FILE 
-chown vagrant:vagrant $KNOW_HOSTS_FILE 
+
 
